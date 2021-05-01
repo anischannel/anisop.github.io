0352003fbbf283ce653d5ba12eb6e867238591a5
anis <73384400+anischannel@users.noreply.github.com>
2021-05-01 19:49:41 +0530

    uptade

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
 
+from flask_frozen import Freezer
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
+
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
